d01365c027e6d533f3e479faa8aebe1ac26f2475
meghanar121 <meghanar121@gmail.com>
2020-05-12 20:29:39 +0200

    Updates

---
+++ precache-manifest.ad4dcc3ae7991abff18fed327e5023e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9a829f1174af420ed1dbd1c8a81bd9",
+    "url": "/credit-card/index.html"
+  },
+  {
+    "revision": "b362c7bc5dff227b8f26",
+    "url": "/credit-card/static/css/2.fc4a1e05.chunk.css"
+  },
+  {
+    "revision": "2de1cfa4773b42b0b055",
+    "url": "/credit-card/static/css/main.1726e588.chunk.css"
+  },
+  {
+    "revision": "b362c7bc5dff227b8f26",
+    "url": "/credit-card/static/js/2.f8a45629.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/credit-card/static/js/2.f8a45629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2de1cfa4773b42b0b055",
+    "url": "/credit-card/static/js/main.2ef9f825.chunk.js"
+  },
+  {
+    "revision": "e03f3e25886a4e797f25",
+    "url": "/credit-card/static/js/runtime-main.af73933a.js"
+  }
+]);
